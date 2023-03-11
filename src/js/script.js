c8bb4f9d94dcf95c1e82d1a55495d714beb9a4ad
MaxBemax <7510332@gmail.com>
2023-03-11 12:15:02 +0200

    add mailer.php

--- src/js/script.js
+++ src/js/script.js
@@ -39,5 +39,66 @@ $(document).ready(function(){
    toggleSlide('.catalog-item__link');
    toggleSlide('.catalog-item__back'); 
 
- });
+   //Modal windows
+
+   $('[data-modal=consultation]').on('click', function () {
+      $('.overlay, #consultation').fadeIn('slow');
+   });
+   $('.modal__close').on('click', function () {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+   });
+
+   $('.button_mini').each(function (i) {
+      $(this).on('click', function () {
+         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+         $('.overlay, #order').fadeIn('slow');
+      })
+   });
+
+   function valideForms(form){
+      $(form).validate({
+         rules: {
+            name: "required",
+            phone: "required",
+            email: {
+               required: true,
+               email: true
+            }
+         },
+         messages: {
+            name: "Будь ласка, введіть ваше ім'я!",
+            phone: "Будь ласка, введіть ваш телефон!",
+            email: {
+              required: "Будь ласка, введіть вашу пошту!",
+              email: "Введіть пошту в потрібному форматі."
+            }
+          }
+      });
+   };
+
+   valideForms('#consultation form');
+   valideForms('#consultation-form');
+   valideForms('#order form');
+
+   $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+   $('form').submit(function(e) {
+      e.preventDefault();
+          if (!$(this).valid()) {      
+          return;
+       }
+      $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize()
+      }).done(function() {
+         $(this).find("input").val("");
+         $('#consultation, #order').fadeOut();
+         $('.overlay, #thanks').fadeIn('slow');
+
+         $('form').trigger('reset');
+      });
+      return false;
+   });
+});
     
